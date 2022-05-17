f16356fefb6764d7795585352440de75bb5948a1
Afzal Zubair <afzal.zubair@khi.iba.edu.pk>
2022-05-17 15:56:06 +0500

    Create task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,20 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
     uint public taskCount = 0;
+    struct Task {
+        uint id;
+        string name;
+        bool done;
+    }
+    
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask('Check out this amaing contract!');
+    }
+
+    function createTask(string memory _name) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _name, false);
+    }
 }
